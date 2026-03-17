8b2ed601ef2110cecb4fa03f9f476c87c6fa9bce
hendry <minixiaopipi@gmail.com>
2026-03-17 17:20:48 +0700

    feat: connecting database to backend

---
+++ app/config/db.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASS"),
+		os.Getenv("POSTGRES_NAME"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_MODE"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
